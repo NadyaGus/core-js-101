5e7e29785402e428fe4f34bec7018473088897bc
NadyaGus <ftotkexit@gmail.com>
2023-10-23 18:01:40 +0300

    feat: add 2 solutions for arrays

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -274,8 +274,9 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, item, index) => (acc.concat(Array.from({ length: (index + 1) },
+    () => item))), []);
 }
 
 
@@ -482,8 +483,9 @@ function getIdentityMatrix(/* n */) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const result = Array.from({ length: (end - start) + 1 }, (value, index) => index + start);
+  return result;
 }
 
 /**
